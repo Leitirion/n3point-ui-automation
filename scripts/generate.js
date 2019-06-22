35fe8ba42f935bc4a557e646b97e04ed1e7db326
Ilnar Gilyaev <gilyaev.if@gmail.com>
2019-06-22 18:34:29 +0500

    add generate #8

---
+++ scripts/generate.js
@@ -0,0 +1,45 @@
+const featureTemplate=(issue='Issue')=>`
+Feature: ${issue} feature
+Background:
+  Given I authed
+
+  @${issue} @focus
+  Scenario: ${issue} scenario
+    Given given ${issue}
+    When when ${issue}
+    Then then ${issue}
+`
+const template =(issue='Issue')=>`
+import { Given,When,Then } from "cypress-cucumber-preprocessor/steps";
+
+const page = {
+    button: {
+        locator: ''
+    }
+}
+
+// arrange
+Given(\`given ${issue}\`, () => {
+    // cy.xpath(page.button.locator).click()
+})
+// act
+When(\`when ${issue}\`, () => {
+})
+// assert
+Then(\`then ${issue}\`, () => {
+})
+`
+const featurename = process.argv[2];
+const  fs = require("fs-extra");
+const featurePath = `./cypress/integration/features/${featurename}.feature`;
+const path = `./cypress/integration/features/${featurename}/${featurename}.js`;
+var data = template(featurename);
+
+fs.outputFile(path, data, (err) => {
+  if (err) console.error(err);
+  fs.writeFile(featurePath, featureTemplate(featurename), (err) => {
+    if (err) console.error(err);
+    console.log(`Successfully generated  featureFile: ${ featureTemplate(featurename)}.`);
+  });
+  console.log(`Successfully generated jsFile: ${path}.`);
+});
